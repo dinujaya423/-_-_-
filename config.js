c6899edaa83f637521af15dc70b0203407e3bc25
dinujaya423 <dinujaya423@gmail.com>
2025-04-14 23:29:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,14 +19,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~Wd0iADra#yKBZaY0gbpBXlALOXD7n1uURs41fvZ28t25hOaLYOsg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "your alive image url",
-MENU_IMG: process.env.MENU_IMG || "your menu pic url",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r8wree.jpg",
+MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/r8wree.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
