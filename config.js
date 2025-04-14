d03d1790ca476a76a361eea93a8743dc7175cdb7
dinujaya423 <dinujaya423@gmail.com>
2025-04-14 23:56:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~Wd0iADra#yKBZaY0gbpBXlALOXD7n1uURs41fvZ28t25hOaLYOsg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r8wree.jpg",
-MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/r8wree.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "your alive image url",
+MENU_IMG: process.env.MENU_IMG || "your menu pic url",
 ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
@@ -38,6 +38,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『DINU-MD 』",
+BOT_NAME: process.env.BOT_NAME || "『YOUR BOTNAME 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
